0751f573d74a0c51b9f0cb3b159619358c30401f
Nefi <nefisantanna@gmail.com>
2020-06-01 21:50:29 -0300

    consumindo dados de uma api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react'; // importando o estado no useState
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'; // importando pacote se for o arquivo tem que por ./
 
 function App(props) {  // todo componente recebe um parametro chamado props de propriedade, {} precisa para inserir codigo js dentro do codigo js, não pode ter dois ou mais jsx retornados, colocando-os dentro de uma div, fragment e uma tag sem nada <>
+  const [ usuario, setUsuario ] = useState(''); // controle do componente
+  function handlePesquisa() {
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+  
+  }
   
   return (
     <>
-      <input name="Usuario" id="Usuario" className="usuarioInput"placeholder="Usuario"/>
-      <button type="button">Pesquisar</button>
+      <p> { usuario } </p>
+
+      <input className="usuarioInput"placeholder="Usuario" value={usuario} onChange = {e => setUsuario(e.target.value)}/>
+
+      <button type="button" onClick = {handlePesquisa}>Pesquisar</button>
     </>
   );
 }
